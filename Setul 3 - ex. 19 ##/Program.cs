4d7ab06c66b9002fdb19db798e0e6fafbe74c934
VieriuAniko <146959409+VieriuAniko@users.noreply.github.com>
2024-01-20 00:43:31 +0200

    Add files via upload

---
+++ Setul 3 - ex. 19 ##/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Setul_3___ex._19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Indicatie: Se da un vector s (vectorul in care se cauta) si un vector p (vectorul care se cauta).
+            //Determinati de cate ori apare p in s. De ex. Daca s = [1,2,1,2,1,3,1,2,1] si p = [1,2,1] atunci p apare in s de 3 ori.
+            //(Problema este dificila doar daca o rezolvati cu un algoritm liniar).
+
+            int nr1, nr2, nr = 0;
+
+            Console.Write("Introduceti cate elemente are vectorul din care se cauta: ");
+            nr1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Introduceti cate elemente are vectorul care se cauta: ");
+            nr2 = int.Parse(Console.ReadLine());
+
+            int[] vectorCautare = new int[nr1];
+            int[] vectorCauta = new int[nr2];
+
+            Console.WriteLine();
+            Console.WriteLine("***Vectorul din care se cauta***");
+            for (int i = 0; i < nr1; i++) 
+            {
+                Console.Write($"Introduceti termenul de pe pozitia {i}: ");
+                vectorCautare[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+
+            Console.Write("vectorCautare are urmatorii termeni: ");
+            for (int i = 0; i < nr1; i++)
+                Console.Write(vectorCautare[i] + " ");
+            Console.WriteLine();
+
+            Console.WriteLine("***Vectorul care se cauta***");
+            for (int i = 0; i < nr2; i++)
+            {
+                Console.Write($"Introduceti termenul de pe pozitia {i}: ");
+                vectorCauta[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+
+            Console.Write("vectorCauta are urmatorii termeni: ");
+            for (int i = 0; i < nr2; i++)
+                Console.Write(vectorCauta[i] + " ");
+            Console.WriteLine();
+            for (int i = 0; i <= nr1 - nr2; i++)
+            {
+                bool gasit = true;
+                for (int j = 0; j < nr2; j++)
+                {
+                    if (vectorCautare[i + j] != vectorCauta[j])
+                    {
+                        gasit = false;
+                        break;
+                    }
+                }
+
+                if (gasit)
+                    nr++;
+            }
+
+            Console.WriteLine($"Vectorul 'vectorCauta' apare de {nr} ori in vectorul 'vectorCautare'");
+        }
+    }
+}
---
+++ Setul 3 - ex. 22 ##/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Linq;
+namespace Setul_3___ex._22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Indicatie: Se dau doi vectori v1 si v2. Se cere sa determine intersectia, reuniunea, si diferentele v1-v2 si v2 -v1
+            //(implementarea operatiilor cu multimi). Elementele care se repeta vor fi scrise o singura data in rezultat. 
+
+            int n, m;
+
+            Console.Write("Numarul de elemente din primul vector: ");
+            n = int.Parse(Console.ReadLine());
+
+            int[] v1 = new int[n];
+
+            Console.Write("Numarul de elemente din al doilea vector: ");
+            m = int.Parse(Console.ReadLine());
+
+            int[] v2 = new int[m];
+
+            Console.Write("***Elementele primului vector***");
+            Console.WriteLine();
+            for(int i = 0; i < n; i++) 
+            {
+                Console.Write($"Introduceti termenul de pe pozitia {i}: ");
+                v1[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Elementele vectorului 'v1' sunt: ");
+            for (int i = 0; i < n; i++)
+                Console.Write(v1[i] + " ");
+
+            Console.WriteLine();
+
+            Console.Write("***Elementele celui de-al doilea vector***");
+            Console.WriteLine();
+            for (int i = 0; i < m; i++)
+            {
+                Console.Write($"Introduceti termenul de pe pozitia {i}: ");
+                v2[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Elementele vectorului 'v2' sunt: ");
+            for (int i = 0; i < m; i++)
+                Console.Write(v2[i] + " ");
+            Console.WriteLine();
+
+            Console.WriteLine("***Intersectia vectorilor 'v1' si 'v2'***");
+            Intersectie(v1, v2);
+            Console.WriteLine();
+
+            Console.WriteLine("***Reuniunea vectorilor 'v1' si 'v2'***");
+            Reuniune(v1, v2);
+            Console.WriteLine();
+
+            Console.WriteLine("***Diferenta vectorilor 'v1' si 'v2'***");
+            Diferenta(v1, v2);
+            Console.WriteLine();
+
+            Console.WriteLine("***Diferenta vectorilor 'v2' si 'v1'***");
+            Diferenta(v2, v1);
+        }
+
+        private static void Diferenta(int[] v1, int[] v2)
+        {
+            var diferenta = v1.Except(v2);
+
+            //Console.Write($"Elementele dupa diferenta sunt: ");
+            foreach (var element in diferenta)
+                Console.Write(element + " ");
+        }
+
+        private static void Reuniune(int[] v1, int[] v2)
+        {
+            var reuniune = v1.Union(v2);
+            var sortat = reuniune.OrderBy(x => x);
+
+            //Console.Write($"Cei doi vectori reuniti alcatuiesc multimea formata din numerele: ");
+            foreach (var element in sortat)
+                Console.Write(element + " ");
+        }
+
+        private static void Intersectie(int[] v1, int[] v2)
+        {
+            var intersectie = v1.Intersect(v2);
+
+            //Console.Write("Elementele comune ale vectorilor sunt: ");
+            foreach (var element in intersectie)
+                Console.Write(element + " ");
+        }
+    }
+}
